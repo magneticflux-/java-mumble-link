cb40fb30125056893d28a8179eb655ee94c8b671
Mitchell Skaggs <skaggsm333@gmail.com>
2019-08-18 15:56:23 -0500

    Update all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.3.31"
+    kotlin("jvm") version "1.3.41"
     id("org.shipkit.java") version "2.2.5"
-    id("com.github.ben-manes.versions") version "0.21.0"
+    id("com.github.ben-manes.versions") version "0.22.0"
 }
 
 group = "com.skaggsm"
@@ -16,15 +16,15 @@ repositories {
 }
 
 dependencies {
-    implementation("com.skaggsm:jvm-shared-memory:0.2.0")
+    implementation("com.skaggsm:jvm-shared-memory:0.2.2")
     implementation("net.java.dev.jna:jna:4.4.0")
     implementation("net.java.dev.jna:platform:3.4.0")
 
     testImplementation(kotlin("stdlib-jdk8"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    testImplementation("org.amshove.kluent:kluent:1.49")
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
-    testImplementation("org.slf4j:slf4j-nop:1.7.26")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.1")
+    testImplementation("org.amshove.kluent:kluent:1.53")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.0")
+    testImplementation("org.slf4j:slf4j-nop:2.0.0-alpha0")
 }
 
 configure<JavaPluginConvention> {

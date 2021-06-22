578a8feadb6f6035e4ce30537ee5c4498a205e19
Mitchell Skaggs <skaggsm333@gmail.com>
2021-06-21 21:26:48 -0500

    Update all dependencies, migrate from JCenter

--- src/test/kotlin/com/skaggsm/jmumblelink/MumbleLinkImplTest.kt
+++ src/test/kotlin/com/skaggsm/jmumblelink/MumbleLinkImplTest.kt
@@ -1,9 +1,12 @@
 package com.skaggsm.jmumblelink
 
-import io.kotlintest.properties.Gen
-import io.kotlintest.properties.assertAll
-import io.kotlintest.specs.StringSpec
-import org.amshove.kluent.`should equal`
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.filter
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
 
 /**
  * Created by Mitchell Skaggs on 5/17/2019.
@@ -16,19 +19,19 @@ class MumbleLinkImplTest : StringSpec({
             it.name = "Testname"
             it.avatarPosition = floatArrayOf(0f, 0f, 0f)
 
-            it.uiVersion `should equal` 2
-            it.uiTick `should equal` 1
-            it.name `should equal` "Testname"
-            it.avatarPosition `should equal` floatArrayOf(0f, 0f, 0f)
+            it.uiVersion shouldBe 2
+            it.uiTick shouldBe 1
+            it.name shouldBe "Testname"
+            it.avatarPosition shouldBe floatArrayOf(0f, 0f, 0f)
         }
     }
 
     "Given a link to Mumble, when a name is written, it should be readable"{
-        assertAll(Gen.string().map { it + it + it }.filter { it.length <= 255 }) { name: String ->
+        checkAll(Arb.string().map { it + it + it }.filter { it.length <= 255 }) { name: String ->
             MumbleLinkImpl().use {
                 it.name = name
 
-                it.name `should equal` name
+                it.name shouldBe name
             }
         }
     }
